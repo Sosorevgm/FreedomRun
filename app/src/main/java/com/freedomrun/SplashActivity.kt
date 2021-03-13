f06af6173c938fa59d48a26a95b9e5f7a3ef7e1e
DESKTOP-L4EUC03\ArtemSh <artem_shendikov@mail.ru>
2021-03-13 12:51:20 +0300

    add logic in splash activity

--- app/src/main/java/com/freedomrun/SplashActivity.kt
+++ app/src/main/java/com/freedomrun/SplashActivity.kt
@@ -1,11 +1,29 @@
 package com.freedomrun
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.view.Window
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.freedomrun.features.main.MainActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        window.requestFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
         setContentView(R.layout.splash_activity)
+        Handler().postDelayed(
+            {
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                finish()
+            },
+            3000
+        )
     }
 }
