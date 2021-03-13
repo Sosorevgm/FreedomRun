0f8e7f2926875960ef5dddb8d45df6046d87764f
MySterioHero <almat101194@gmail.com>
2021-03-14 04:07:19 +0600

    the part of the current position has done ? key_api wah hidden temporarily

---
+++ app/src/main/java/com/freedomrun/features/main/IGoogleAPIService.kt
@@ -0,0 +1,10 @@
+package com.freedomrun.features.main
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Url
+
+interface IGoogleAPIService {
+    @GET
+    fun getNearbyPlaces(@Url url: String): Call<>
+}
